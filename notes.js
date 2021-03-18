e7649c228ec41ff61312745b4ee5d2fc6ff90eca
unknown <codewithsrobins1@gmail.com>
2021-03-18 16:52:07 -0700

    update to folder

---
+++ notes.js
@@ -0,0 +1,19 @@
+//Why TypeScript?
+    //Erros in compiling stage and not run time
+    //Able to scale applications - Able to define data -- components must have these propertie
+
+//TypeScript is an extension/superset of JavaScript
+    //Will transpile down to JS
+
+
+//Build a Create-TypeScript-App
+
+// npx create-react-app [app-name] --template typescript
+
+
+
+//ESLINT SETUP
+
+// https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md
+
+// npx eslint . --ext .js,.jsx,.ts,.tsx
